407e96f05f09c57b1c60fac03736953c14dd8854
moschidisg <mosxidismosxidis@gmail.com>
2022-11-19 19:51:54 +0200

    create component for displaying tasks

---
+++ src/components/Overview.js
@@ -0,0 +1,21 @@
+// RENDERS TASKS
+import React from 'react';
+import uniqid from 'uniqid';
+
+const Overview = (props) => {
+    const { tasks } = props;
+
+    return (
+        <ul>
+            {tasks.map((task, index) => {
+                return (
+                    <li key={task.id}>
+                        {index} {task.text}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export default Overview;
